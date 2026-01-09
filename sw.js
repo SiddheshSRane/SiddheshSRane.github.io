02e22c3f82a3cabb6b04d1ee53c1de524c47974e
Siddhesh Rane <siddhesh.sh.rane@gmail.com>
2026-01-10 01:34:46 +0530

    Optimize portfolio: PWA support, performance improvements, and accessibility fixes

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'portfolio-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/critical.css',
+    '/css/custom.css',
+    '/js/app.min.js',
+    '/manifest.json',
+    '/images/icon-192.png?v=2',
+    '/images/icon-512.png?v=2'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
